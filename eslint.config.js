a8faad134f87aa60f97e42583e66c594b3fcd28c
Timur Atalay <timur@atalay.io>
2026-04-29 23:50:10 -0700

    add documentation files to prettier and eslint ignore lists

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,7 @@ import globals from 'globals'
 import js from '@eslint/js'
 
 export default [
-  { ignores: ['dist'] },
+  { ignores: ['dist', 'docs/daterangepicker.css', 'docs/daterangepicker.min.js', 'docs/daterangepicker.min.js.map'] },
   js.configs.recommended,
   {
     languageOptions: {
